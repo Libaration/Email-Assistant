9e42311f456508c301dd38de4af16d3aa73e9e79
Cristian Deleon <libaration@gmail.com>
2021-10-27 20:07:30 -0400

    Finish side nav bar icons /styling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import logo from './logo.svg';
 import { Flex, Box } from '@chakra-ui/react';
 import SideBar from './components/SideBar.jsx';
+import { useState } from 'react';
 function App() {
+  const [page, setPage] = useState();
   return (
     <>
       <Box w="100vw" h="35px" sx={{ '-webkit-app-region': 'drag' }}>
-        <Box h="100%" bg="#203d85" w="75px" />
+        <Box
+          h="100%"
+          bg="#203d85"
+          w="76px"
+          borderRight="1px solid"
+          borderColor="gray.800"
+        />
       </Box>
       {/*there's probably a better way to the above. i hope when i come back to this i'll have found it out*/}
       <Flex>
-        <SideBar />
-        <Box w="100%" h="100%">
+        <SideBar setPage={setPage} />
+        <Box w="100%" h="100%" pl={5}>
           CONTENT
         </Box>
       </Flex>
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
-import { Box, Flex } from '@chakra-ui/react';
-
-export default function SideBar() {
+import { Image, Flex, List, ListItem, Box } from '@chakra-ui/react';
+import home from '../assets/icons/home.png';
+import email from '../assets/icons/email.png';
+import settings from '../assets/icons/settings.png';
+import logo from '../assets/icons/logo.png';
+import { motion } from 'framer-motion';
+export default function SideBar(props) {
+  const MotionBox = motion(Box);
+  const menuItem = (src) => {
+    return (
+      <MotionBox
+        mt={5}
+        mb={5}
+        whileHover={{ scale: 1.2, filter: 'invert(1)', cursor: 'pointer' }}
+        sx={{ filter: 'invert(80%)' }}
+      >
+        <Image src={src} />
+      </MotionBox>
+    );
+  };
   return (
-    <Box
+    <Flex
       minWidth="76px"
       maxWidth="76px"
       bg="#203d85"
@@ -11,10 +28,14 @@ export default function SideBar() {
       h="100vh"
       borderRight="1px solid"
       borderColor="gray.800"
+      flexDirection="column"
     >
-      testing nav
-      <br />
-      ok
-    </Box>
+      <Image src={logo} p={3} pr={1} />
+      <List pl={5} pr={5} h="100%">
+        <ListItem>{menuItem(home)}</ListItem>
+        <ListItem>{menuItem(email)}</ListItem>
+        <ListItem>{menuItem(settings)}</ListItem>
+      </List>
+    </Flex>
   );
 }
