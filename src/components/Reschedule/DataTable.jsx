c1fc63256bc78a0492720705b6a4a345ba9d6465
Libaration <libaration@gmail.com>
2024-03-10 21:30:54 -0400

    📝 (DataTable.jsx): Add getPaginationRowModel import and Button component import for pagination functionality 📝 (DataTable.jsx): Update DataTable component to include pagination functionality with previous and next buttons 📝 (columns.jsx): Add new columns for "Expired" and "Expires In" with dynamic color based on expiration date and status value

--- src/components/Reschedule/DataTable.jsx
+++ src/components/Reschedule/DataTable.jsx
@@ -2,7 +2,9 @@ import {
   flexRender,
   getCoreRowModel,
   useReactTable,
+  getPaginationRowModel,
 } from "@tanstack/react-table";
+import { Button } from "@/components/ui/button";
 
 import {
   Table,
@@ -18,6 +20,7 @@ export function DataTable({ columns, data }) {
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
   });
 
   const renderCellValue = (cell) => {
@@ -38,45 +41,68 @@ export function DataTable({ columns, data }) {
   };
 
   return (
-    <div className="rounded-md border">
-      <Table>
-        <TableHeader>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <TableRow key={headerGroup.id}>
-              {headerGroup.headers.map((header) => (
-                <TableHead key={header.id}>
-                  {header.isPlaceholder
-                    ? null
-                    : flexRender(
-                      header.column.columnDef.header,
-                      header.getContext(),
-                    )}
-                </TableHead>
-              ))}
-            </TableRow>
-          ))}
-        </TableHeader>
-        <TableBody>
-          {table.getRowModel().rows?.length ? (
-            table.getRowModel().rows.map((row) => (
-              <TableRow
-                key={row.id}
-                data-state={row.getIsSelected() && "selected"}
-              >
-                {row.getVisibleCells().map((cell) => (
-                  <TableCell key={cell.id}>{renderCellValue(cell)}</TableCell>
+    <div>
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => (
+                  <TableHead key={header.id}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                        header.column.columnDef.header,
+                        header.getContext(),
+                      )}
+                  </TableHead>
                 ))}
               </TableRow>
-            ))
-          ) : (
-            <TableRow>
-              <TableCell colSpan={columns.length} className="h-24 text-center">
-                No results.
-              </TableCell>
-            </TableRow>
-          )}
-        </TableBody>
-      </Table>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>{renderCellValue(cell)}</TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <div className="flex items-center justify-end space-x-2 py-4">
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() => table.previousPage()}
+          disabled={!table.getCanPreviousPage()}
+        >
+          Previous
+        </Button>
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() => table.nextPage()}
+          disabled={!table.getCanNextPage()}
+        >
+          Next
+        </Button>
+      </div>
     </div>
   );
 }
--- src/components/Reschedule/columns.jsx
+++ src/components/Reschedule/columns.jsx
@@ -1,22 +1,103 @@
+import moment from "moment";
+const expiredColor = "text-yellow-100";
+
 export const columns = [
   {
     accessorKey: "street",
     header: "Street",
+    cell: ({ row }) => (
+      <div className={row.original.expired.value && expiredColor}>
+        {row.original.street}
+      </div>
+    ),
   },
   {
     accessorKey: "city",
     header: "City",
+    cell: ({ row }) => (
+      <div className={row.original.expired.value && expiredColor}>
+        {row.original.city}
+      </div>
+    ),
   },
   {
     accessorKey: "state",
     header: "State",
+    cell: ({ row }) => (
+      <div className={row.original.expired.value && expiredColor}>
+        {row.original.state}
+      </div>
+    ),
   },
   {
     accessorKey: "zipcode",
     header: "Zip",
+    cell: ({ row }) => (
+      <div className={row.original.expired.value && expiredColor}>
+        {row.original.zipcode}
+      </div>
+    ),
+  },
+  {
+    accessorKey: "expired.value",
+    header: "Expired",
+    cell: ({ row }) => (
+      <div className={row.original.expired.value ? expiredColor : ""}>
+        {row.original.expired.value ? "Yes" : "No"}
+      </div>
+    ),
   },
   {
     accessorKey: "previousAuctions.totalCount",
     header: "Prev Listings",
-  }
-]
+    cell: ({ row }) => (
+      <div className={row.original.expired.value && expiredColor}>
+        {row.original.previousAuctions.totalCount}
+      </div>
+    ),
+  },
+  {
+    accessorKey: "expirationDate",
+    header: "Expires In",
+    cell: ({ row }) => {
+      const expirationDate = moment(
+        row.original.expirationDate.value,
+        "YYYY-MM-DD",
+      ).endOf("day");
+      const yesterday = moment().subtract(1, "days").startOf("day");
+      const daysRemaining = expirationDate.diff(yesterday, "days");
+
+      // Calculate color intensity based on remaining days, starting dynamic coloring after a week
+      const colorIntensity =
+        daysRemaining > 7
+          ? 0
+          : Math.max(
+            0,
+            Math.min(100, Math.floor((7 - daysRemaining) * (100 / 7))),
+          );
+
+      const textColor =
+        daysRemaining > 7
+          ? ""
+          : `rgb(255, ${255 - colorIntensity + 150}, ${255 - colorIntensity})`; // Dynamic color for days, no color for anything past a week, and maintain red for "Expired"
+
+      return (
+        <div
+          style={{ color: daysRemaining > 0 ? textColor : "rgb(252 165 165)" }}
+        >
+          {daysRemaining > 0 ? `${daysRemaining} days` : "Expired"}
+        </div>
+      );
+    },
+  },
+
+  {
+    accessorKey: "status.value",
+    header: "Status",
+    cell: ({ row }) => {
+      const expired = row.original.expired.value;
+      const status = expired ? "Waiting" : row.original.status.value;
+      return <div className={expired && expiredColor}>{status}</div>;
+    },
+  },
+];
